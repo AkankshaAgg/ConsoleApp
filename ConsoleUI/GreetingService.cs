6c026a48abd3afed9bad66a4b991be03b1c21f02
AkankshaAgg <40016511+AkankshaAgg@users.noreply.github.com>
2020-11-06 10:11:56 +0530

    Learning

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+//DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> _log;
+        private readonly IConfiguration _Config;
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            _log = log;
+            _Config = config;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < _Config.GetValue<int>("LoopTimes"); i++)
+            {
+                _log.LogInformation("Run number {runNumber}", i);
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+//DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup connection to first our configuration 
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            //setup work for our logger (Serilog)
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    //DEPENDENCY INJECTION
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            svc.Run();
+        }
+
+        //we wanna log right away 
+        //required for serilog
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            //sets up our talking to configuration source
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables(); //they can override things in appsettings.json
+        }
+    }
+}
